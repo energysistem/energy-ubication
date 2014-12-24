7dc1cad72cee74c203fbdc633ab1e7708f4de3df
Flavio Corpa Ríos <flaviocorpa@gmail.com>
2014-12-24 13:33:43 +0100

    Internationalize Main.java

--- EnergyUbication/src/android/ubication/Main.java
+++ EnergyUbication/src/android/ubication/Main.java
@@ -1,5 +1,4 @@
 package android.ubication;
-
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,45 +16,44 @@ import android.widget.Toast;
 
 /**
  * 
- * @author Flavio Corpa Ríos.
+ * @author Flavio Corpa RÃ­os.
  *
  */
 public class Main extends Activity {
 	
-	//Atributos
-	TextView edtLatitud;
-	TextView edtLongitud;
-	TextView edtPrecision;
-	TextView edtEstadoProveedor;
-	Button btnActivar;
-	Button btnDesactivar;
+	// Attributes
+	TextView edtLatitude;
+	TextView edtLongitude;
+	TextView edtAccuracy;
+	TextView edtProviderStatus;
+	Button btnEnable;
+	Button btnDisable;
 	
-	LocationManager gestorLocalizacion;
-	LocationListener locEscuchador;
+	LocationManager locManager;
+	LocationListener locListener;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		edtLatitud = (TextView) this.findViewById(R.id.edtLatitud);
-		edtLongitud = (TextView) this.findViewById(R.id.edtLongitud);
-		edtPrecision = (TextView) this.findViewById(R.id.edtPrecision);
-		edtEstadoProveedor = (TextView) this.findViewById(R.id.edtEstadoProveedor);
-		btnActivar = (Button) this.findViewById(R.id.btnActivar);
-		btnDesactivar = (Button) this.findViewById(R.id.btnDesactivar);
+		edtLatitude = (TextView) this.findViewById(R.id.edtLatitude);
+		edtLongitude = (TextView) this.findViewById(R.id.edtLongitude);
+		edtAccuracy = (TextView) this.findViewById(R.id.edtAccuracy);
+		edtProviderStatus = (TextView) this.findViewById(R.id.edtProviderStatus);
+		btnEnable = (Button) this.findViewById(R.id.btnEnable);
+		btnDisable = (Button) this.findViewById(R.id.btnDisable);
 
-        btnActivar.setOnClickListener(new OnClickListener() {
+	        btnEnable.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				localizar();
+				locate();
 			}
 		});
-        
-        btnDesactivar.setOnClickListener(new OnClickListener() {
+	        
+	        btnDisable.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-		    	gestorLocalizacion.removeUpdates(locEscuchador);
+		    		locManager.removeUpdates(locListener);
 			}
 		});   
 	}
@@ -66,131 +64,67 @@ public class Main extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-    private void localizar()
-    {
-    	//Obtenemos una referencia al LocationManager
-    	gestorLocalizacion = 
-    		(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-    	
-    	//Obtenemos la última posición conocida
-    	Location localizacion = 
-    		gestorLocalizacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    	
-    	//Mostramos la última posición conocida
-    	mostrarPosicion(localizacion);
-    	
-    	//Si el GPS está deshabilitado
-		//if (!gestorLocalizacion.isProviderEnabled(LocationManager.GPS_PROVIDER))
-			//advertirGPSdeshabilitado();
+	
+	private void locate()
+	{
+	    	// Reference to LocationManager
+	    	locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	    	
+	    	Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	    	
+	    	showPosition(location);
+	    	
+		if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+			warnGPSdisabled();
 		
-		//Si no hay cobertura
-		if (!gestorLocalizacion.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
-			advertir3Gdeshabilitado();
-    	
-    	//Nos registramos para recibir actualizaciones de la posición
-    	locEscuchador = new LocationListener() {
-	    	public void onLocationChanged(Location localizacion) {
-	    		mostrarPosicion(localizacion);
-	    	}
-	    	public void onProviderDisabled(String proveedor){
-	    		edtEstadoProveedor.setText("Proveedor desconectado");
-	    	}
-	    	public void onProviderEnabled(String proveedor){
-	    		edtEstadoProveedor.setText("Proveedor conectado");
-	    	}
-	    	public void onStatusChanged(String proveedor, int estado, Bundle extras){
-	    		Log.i("", "Estado del proveedor: " + estado);
-	    		edtEstadoProveedor.setText("Estado del proveedor: " + estado);
-	    	}
-    	};
-    	
-    	//Si el GPS está habilitado, usa la ubicación del GPS
-    	if (gestorLocalizacion.isProviderEnabled(LocationManager.GPS_PROVIDER))
-    		gestorLocalizacion.requestLocationUpdates(
-	    			LocationManager.GPS_PROVIDER, 30000, 0, locEscuchador);
-    	else //Si no, utiliza la ubicación de la red móvil.
-	    	gestorLocalizacion.requestLocationUpdates(
-	    			LocationManager.NETWORK_PROVIDER, 30000, 0, locEscuchador);
-    }
-     
-    private void mostrarPosicion(Location localizacion) {
-    	if (localizacion!=null) {
-    		edtLatitud.setText("Latitud: " + String.valueOf(localizacion.getLatitude()));
-    		edtLongitud.setText("Longitud: " + String.valueOf(localizacion.getLongitude()));
-    		edtPrecision.setText("Precisión: " + String.valueOf(localizacion.getAccuracy()));
+		if (!locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+			warn3Gdisabled();
+	    	
+	    	// Set listener to update location
+	    	locListener = new LocationListener() {
+		    	public void onLocationChanged(Location loc) {
+		    		showPosition(loc);
+		    	}
+		    	public void onProviderDisabled(String provider){
+		    		edtProviderStatus.setText("Provider desconectado");
+		    	}
+		    	public void onProviderEnabled(String provider){
+		    		edtProviderStatus.setText("Provider conectado");
+		    	}
+		    	public void onStatusChanged(String provider, int status, Bundle extras){
+		    		edtProviderStatus.setText("Provider status changed to: " + status);
+		    	}
+	    	};
+	    	
+	    	// If GPS is enabled, take ubication from it, if not, take the phone network one
+	    	if (locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+	    		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, locListener);
+	    	else
+	    		locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000, 0, locListener);
     	}
-    	else {
-    		edtLatitud.setText("Latitud: (Sin datos)");
-    		edtLongitud.setText("Longitud: (Sin datos)");
-    		edtPrecision.setText("Precisión: (Sin datos)");    		
+    	
+    	private void showPosition(Location loc) {
+	    	if (loc != null) {
+	    		edtLatitude.setText("Latitude: " + String.valueOf(loc.getLatitude()));
+	    		edtLongitude.setText("Longitude: " + String.valueOf(loc.getLongitude()));
+	    		edtAccuracy.setText("Accuracy: " + String.valueOf(loc.getAccuracy()));
+	    	}
+	    	else {
+	    		edtLatitude.setText("Latitude: (undefined)");
+	    		edtLongitude.setText("Longitude: (undefined)");
+	    		edtAccuracy.setText("Accuracy: (undefined)");    		
+	    	}
     	}
-    }
+    	
+    	public void warnGPSdisabled()
+    	{
+    		Toast msg = Toast.makeText(this, "GPS is disabled!", Toast.LENGTH_SHORT);
+		msg.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0).show();
+   	}
     
-    
-    public void advertirGPSdeshabilitado()
-    {
-    	Toast mensaje = Toast.makeText(this, "¡El GPS está deshabilitado!", Toast.LENGTH_SHORT);
-		mensaje.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0);
-		mensaje.show();	
-    }
-    
-    public void advertir3Gdeshabilitado()
-    {
-    	Toast mensaje = Toast.makeText(this, "¡El Operador de Telefonía está desconectado!", Toast.LENGTH_SHORT);
-		mensaje.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0);
-		mensaje.show();	
-    }
+    	public void warn3Gdisabled()
+    	{
+    		Toast msg = Toast.makeText(this, "Network provider is disabled!", Toast.LENGTH_SHORT);
+		msg.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0).show();
+    	}
 }
-
-/*
-	//Obtenemos una referencia al LocationManager que es la clase principal en la que
-	//nos basaremos siempre a la hor de utilizar la API de localización de Android.
-	LocationManager gestorLocalizacion = (LocationManager)getSystemService(LOCATION_SERVICE);
-	
-	
-	//Obtenemos la lista de todos los proveedores.
-	List<String> listaProveedores = gestorLocalizacion.getAllProviders();
-	
-	//A continuación vamos a obtener las propiedades del primero de los proveedores obtenidos.
-	//Obtenemos una referencia al primer proveedor.
-	LocationProvider proveedor = gestorLocalizacion.getProvider(listaProveedores.get(0));
-	//Obtenemos la propiedad de la precisión del proveedor.
-	int precision = proveedor.getAccuracy();
-	//Obtenemos la propiedad que nos indica si obtiene la altitud.
-	boolean obtieneAltitud = proveedor.supportsAltitude();
-	//Obtenemos la propiedad del nivel de consumo de recursos del proveedor.
-	int consumoRecursos = proveedor.getPowerRequirement();
-	
-	//Creamos el objeto de la clase Criteria
-	Criteria criterio = new Criteria();
-	//Especificamos precisión alta
-	criterio.setAccuracy(Criteria.ACCURACY_FINE);
-	//Especificamos que proporcione la altitud
-	criterio.setAltitudeRequired(true);
-	
-	//Mejor proveedor que cumple los criterios especificados
-	String mejorProveedorCriterio = gestorLocalizacion.getBestProvider(criterio, false);
-	//Lista de proveedores que cumplen los criterios especificados
-	List<String> listaProveedoresCriterio = gestorLocalizacion.getProviders(criterio, false);
-	
-	//Si el GPS no está habilitado
-	if (!gestorLocalizacion.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-		advertirGPSdeshabilitado();
-	}
-	
-	LocationListener locEscuchador = new LocationListener() {
-		public void onLocationChanged(Location localizacion) {
-			mostrarPosicion(localizacion);
-		}
-		public void onProviderDisabled(String proveedor) {
-			edtEstadoProveedor.setText("Proveedor Apagado");
-		}
-		public void onProviderEnabled(String proveedor) {
-			edtEstadoProveedor.setText("Proveedor Encendido");
-		}
-		public void onStatusChanged(String proveedor, int estado, Bundle extras) {
-			edtEstadoProveedor.setText("Estado del Proveedor: " + estado);
-		}
-	};
-*/
